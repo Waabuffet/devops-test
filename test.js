21ded7ad4750e1a3d6c7bc796621f9c461709bf4
Dominique <dominousas@gmail.com>
2021-05-08 14:34:30 +0300

    made test using junit chai module

---
+++ test.js
@@ -0,0 +1,22 @@
+var env = require('./env/env');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+
+var respOKstring = 'Hello World';
+var server = env.method + '://' + env.host + ':' + env.port;
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('Website API test', () => {
+    it('It should return a json property response equals to string "' + respOKstring + '"', (done) => {
+        chai.request(server).get(env.api).end((err, resp) => {
+            resp.should.have.status(200);
+            resp.body.should.be.a('object', "Response body is not an object");
+            resp.body.should.have.property('response');
+            resp.body.response.should.be.a('string', "Website api response is not a string");
+            resp.body.response.should.equal(respOKstring, "Website api returns wrong string");
+            done();
+        });
+    });
+});
